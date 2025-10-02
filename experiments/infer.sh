60dfb5fbb4fd02735dac9a798ff371335ec14e5c
Yasheng Sun <suny0a@kl-26595.kaust.edu.sa>
2025-10-02 13:36:18 +0300

    up

--- experiments/infer.sh
+++ experiments/infer.sh
@@ -1,8 +1,10 @@
 ## Generate 3D MOF structures from preds_samples-5.json
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict.py \
     inference.task=gen \
-    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/epoch_1-step_28038-loss_2.7838.ckpt \
+    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/gen/sp_module/epoch_200-step_414498-loss_0.8644.ckpt \
     +inference.gen.metal_lib_path=/ibex/user/suny0a/Proj/MOFFlow-2/data/metals/metal_lib_train.pkl \
     +inference.gen.mof_seqs_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/inference/temp_1.0_unconditional/preds_samples-5.json \
     inference.num_samples=1 \
     inference.sampler.num_timesteps=50
+
+# inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/epoch_1-step_28038-loss_2.7838.ckpt \
