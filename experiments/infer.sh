a396118ee2b1d949fd42eef140b6c357a351bbc0
Yasheng Sun <suny0a@KL-26595.local>
2025-10-02 11:00:30 +0300

    up

--- experiments/infer.sh
+++ experiments/infer.sh
@@ -3,6 +3,6 @@ PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict.py \
     inference.task=gen \
     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/epoch_1-step_28038-loss_2.7838.ckpt \
     +inference.gen.metal_lib_path=/ibex/user/suny0a/Proj/MOFFlow-2/data/metals/metal_lib_train.pkl \
-    +inference.gen.mof_seqs_path=/ibex/user/suny0a/Proj/preds_samples-5.json \
+    +inference.gen.mof_seqs_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/inference/temp_1.0_unconditional/preds_samples-5.json \
     inference.num_samples=1 \
     inference.sampler.num_timesteps=50
