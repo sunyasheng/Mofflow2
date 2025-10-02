59866fc9767ff3d7663f81c05a246ac06a5f79c5
Yasheng Sun <suny0a@KL-26595.local>
2025-10-02 10:36:45 +0300

    up

--- experiments/infer.sh
+++ experiments/infer.sh
@@ -1,16 +1,9 @@
-# python experiments/predict.py \
-#     inference.task=csp \
-#     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt \ # default to null
-#     inference.num_samples=1 \   # default to 1
-#     inference.num_devices=1 \  
-#     inference.sampler.num_timesteps=50
+## CSP
 
 
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
-    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/last.ckpt \
-    inference.total_samples=5
-
-# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict.py \
-#     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/last.ckpt \
-#     inference.num_samples=10 \
-#     data.test_sample_limit=10
+    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/seq/ckpt/epoch_1-step_28038-loss_2.7838.ckpt \
+    inference.gen.metal_lib_path=/ibex/user/suny0a/Proj/MOFFlow-2/data/metals/metal_lib_train.pkl \
+    inference.gen.mof_seqs_path=/ibex/user/suny0a/Proj/preds_samples-5.json \
+    inference.total_samples=1000 \
+    inference.temperature=1.0
