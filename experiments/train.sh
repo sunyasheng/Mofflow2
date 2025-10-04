1c91f213b45d4cc5add6217403cbb294d7fe96ba
Yasheng Sun <suny0a@KL-26595.local>
2025-10-04 19:44:05 +0300

    continue training

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,1 +1,2 @@
-PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp > output.log 2>&1
+# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp > output.log 2>&1
+PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/csp/ckpt/epoch_4-step_70093-loss_2.2509.ckpt > output.log 2>&1
