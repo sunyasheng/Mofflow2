8cdbda3047a0bc6b41b7978c9e7a40ffc34e4bf6
Yasheng Sun <suny0a@kl-26595.kaust.edu.sa>
2025-10-08 14:34:50 +0300

    infer seq

--- experiments/infer_seq.sh
+++ experiments/infer_seq.sh
@@ -1,5 +1,5 @@
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
-    inference.total_samples=5
+    inference.total_samples=250
 
 # logs/mof-seq/seq_1step/inference/temp_1.0_unconditional/preds_samples-5.json
