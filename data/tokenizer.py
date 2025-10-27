e5526030ac134e7d0a56dde615397147755c0662
Yasheng Sun <suny0a@KL-26595.local>
2025-10-27 12:08:21 +0300

    up

--- data/tokenizer.py
+++ data/tokenizer.py
@@ -12,6 +12,7 @@ class SmilesTokenizer:
         self._finalized = False
 
     def tokenize(self, sequence):
+        import pdb; pdb.set_trace()
         tokens = []
         for part in sequence.strip().split():
             if part in self.special_tokens:
