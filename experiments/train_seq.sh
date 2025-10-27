94e543b3a9b2678cccbef9c33e31498942078225
Yasheng Sun <suny0a@KL-26595.local>
2025-10-27 11:57:46 +0300

    up

--- experiments/train_seq.sh
+++ experiments/train_seq.sh
@@ -2,6 +2,8 @@
 #   experiment.name=seq_1step \
 #   2>&1 | tee output_seq.log
 
+cd  /ibex/user/suny0a/Proj/MOFFlow-2
+source .env
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train_seq.py \
   experiment.name=seq_1step_conditional model.conditional=true \
   2>&1 | tee output_seq.log
