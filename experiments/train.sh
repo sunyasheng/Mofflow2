520d9bf4a7189e45d8f473d44bc810bd7bcf3552
Yasheng Sun <suny0a@kl-26595.kaust.edu.sa>
2025-11-13 14:26:21 +0300

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -1,2 +1,4 @@
 # PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp > output.log 2>&1
-PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/csp/ckpt/epoch_4-step_70093-loss_2.2509.ckpt > output.log 2>&1
+# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/csp/ckpt/epoch_4-step_70093-loss_2.2509.ckpt > output.log 2>&1
+
+PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=gen experiment.name=csp_$(date +%Y%m%d_%H%M%S) > output.log 2>&1
