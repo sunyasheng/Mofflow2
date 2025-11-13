6e55aee746257ff7c3e020f3ca58c5035dbc0469
Yasheng Sun <suny0a@kl-26595.kaust.edu.sa>
2025-11-13 16:00:47 +0300

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -2,7 +2,7 @@
 # PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=csp experiment.name=csp experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-csp/csp/ckpt/epoch_4-step_70093-loss_2.2509.ckpt > output.log 2>&1
 # 训练的时候只是数据加载不一样 gen，csp所在的文件路径不一样
 # 添加调试输出，不重定向到 log 文件，直接看输出
-PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=gen experiment.name=csp_$(date +%Y%m%d_%H%M%S) 2>&1 | tee output.log
+PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py experiment.task=gen experiment.name=csp_$(date +%Y%m%d_%H%M%S) 2>&1 | tee output_$(date +%Y%m%d_%H%M%S).log
 
 # 或者先用调试模式测试（减少 workers，小数据集）
 # PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py \
