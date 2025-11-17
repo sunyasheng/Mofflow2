948b366d05526a70e865468a800c2149e24d7d56
Yasheng Sun <suny0a@kl-26595.kaust.edu.sa>
2025-11-17 12:50:45 +0300

    up

--- experiments/infer_seq.sh
+++ experiments/infer_seq.sh
@@ -1,9 +1,17 @@
-# original
+
+
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
-    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
+    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/gen/seq_module_unconditional/epoch_20-step_10728-loss_0.1081.ckpt \
     inference.total_samples=1000
 
-# ours
+
+# ours unconditional trained with their code
+# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
+#     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
+#     inference.total_samples=1000
+
+
+# ours unconditional trained with our code
 # PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
 #     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step_conditional/ckpt/epoch_19-step_45279-loss_0.1291.ckpt \
 #     inference.total_samples=250
