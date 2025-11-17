e68de5b0597a47a0eb84e3e9f231ad764d3e040e
Yasheng Sun <suny0a@KL-26595.local>
2025-11-17 11:53:50 +0300

    up

--- experiments/infer_seq.sh
+++ experiments/infer_seq.sh
@@ -1,11 +1,13 @@
-# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
-#     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
-#     inference.total_samples=250
-
+# original
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
-    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step_conditional/ckpt/epoch_19-step_45279-loss_0.1291.ckpt \
+    inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
     inference.total_samples=250
-    # inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
+
+# ours
+# PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
+#     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step_conditional/ckpt/epoch_19-step_45279-loss_0.1291.ckpt \
+#     inference.total_samples=250
+#     # inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
 
 
 # logs/mof-seq/seq_1step/inference/temp_1.0_unconditional/preds_samples-5.json
