33d5a3614e6bee5e431a7bc56ea4cf7616922056
Yasheng Sun <suny0a@KL-26595.local>
2025-11-17 11:55:38 +0300

    up

--- experiments/infer_seq.sh
+++ experiments/infer_seq.sh
@@ -1,7 +1,7 @@
 # original
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
     inference.ckpt_path=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-seq/seq_1step/ckpt/last.ckpt \
-    inference.total_samples=250
+    inference.total_samples=1000
 
 # ours
 # PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/predict_seq.py \
