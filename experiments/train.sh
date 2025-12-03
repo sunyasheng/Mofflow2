b7507df22cb032c0798e83621a757a82de354f15
Yasheng Sun <suny0a@KL-26595.local>
2025-12-03 12:57:05 +0300

    up

--- experiments/train.sh
+++ experiments/train.sh
@@ -6,9 +6,11 @@
 PYTHONPATH=/ibex/user/suny0a/Proj/MOFFlow-2 python experiments/train.py \
   experiment.task=gen \
   experiment.name=csp_$(date +%Y%m%d_%H%M%S) \
-  experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-gen/csp_20251120_125600/ckpt/epoch_14-step_139364-loss_3.9131.ckpt \
+  experiment.warm_start=/ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-gen/csp_20251129_095557/ckpt/epoch_69-step_650404-loss_1.3757.ckpt \
   2>&1 | tee output_$(date +%Y%m%d_%H%M%S).log
 
+
+# /ibex/user/suny0a/Proj/MOFFlow-2/logs/mof-gen/csp_20251120_125600/ckpt/epoch_14-step_139364-loss_3.9131.ckpt
 
 # ========== 多 GPU 使用方法 ==========
 # 使用多个 GPU：在命令行中添加 experiment.num_devices=N (N 为 GPU 数量)
